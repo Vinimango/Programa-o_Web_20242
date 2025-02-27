b535005f2d2eaef0a372bbcd6799d00acb7c0934
Vinícius Mango <141795361+Vinimango@users.noreply.github.com>
2025-02-27 16:16:49 -0300

    Delete projeto directory

--- projeto/routes/portfolio.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const portfolioRouter = express.Router();
-
-portfolioRouter.get("/", (req, res) => {
-  res.render("portfolio"); // Remove `res.send()`
-});
-
-export default portfolioRouter;
